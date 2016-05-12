6536cb44169bbcebdf079ebbf1a2a1cc6020d2d7
Phil Ross <philippross369@gmail.com>
2016-05-12 14:07:06 -0400

    added readfastx function to utils.py

--- utils.py
+++ utils.py
@@ -48,4 +48,46 @@ def print_fasta(fasta_dict, width=60):
   for key in d.keys():
     id = ">" + key
     seq = "\n".join(textwrap.wrap(d[key], width))
-    sys.stdout.write(id + "\n" + seq + "\n")
+
+# Originally written by Heng Li (https://github.com/lh3/readfq)
+# Almost certainly faster than my alternative
+def readfastx(fasta_file): # this is a generator function
+  """Usage:
+  n, slen, qlen = 0, 0, 0
+  for name, seq, qual in readfastx(sys.stdin):
+    n += 1$
+    slen += len(seq)$
+    qlen += qual and len(qual) or 0$
+    print n, '\t', slen, '\t', qlen    sys.stdout.write(id + "\n" + seq + "\n")$   
+  
+  """ 
+  with open(fasta_file) as fp:
+    last = None # this is a buffer keeping the last unprocessed line
+    while True: # mimic closure; is it a bad idea? 
+      if not last: # the first record or a record following a fastq
+        for l in fp: # search for the start of the next record
+          if l[0] in '>@': # fasta/q header line
+            last = l[:-1] # save this line
+            break
+            if not last: break
+            name, seqs, last = last[1:].partition(" ")[0], [], None
+            for l in fp: # read the sequence
+              if l[0] in '@+>':
+                last = l[:-1]
+                  break
+              seqs.append(l[:-1])
+          if not last or last[0] != '+': # this is a fasta record
+              yield name, ''.join(seqs), None # yield a fasta record
+              if not last: break
+          else: # this is a fastq record
+              seq, leng, seqs = ''.join(seqs), 0, []
+              for l in fp: # read the quality
+                  seqs.append(l[:-1])
+                  leng += len(l) - 1
+                  if leng >= len(seq): # have read enough quality
+                      last = None
+                      yield name, seq, ''.join(seqs); # yield a fastq record
+                      break
+              if last: # reach EOF before reading enough quality
+                  yield name, seq, None # yield a fasta record instead
+                  break
